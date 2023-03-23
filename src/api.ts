f0e25c0e99cb97a42b852e34895ec54c967c9f67
seheonnn <ho78901@naver.com>
2023-03-23 15:55:03 +0900

    feat: LogIn cookie & LogOut

--- src/api.ts
+++ src/api.ts
@@ -10,7 +10,8 @@ import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/v1/",
+    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    withCredentials: true,
 });
 
 export const getRooms = () => 
@@ -27,3 +28,9 @@ export const getRoomReviews = ( {queryKey} : QueryFunctionContext ) => {
     const [_, roomPk] = queryKey;
     return instance.get(`rooms/${roomPk}/reviews`).then((response) => response.data);
 };
+
+export const getMe = () => 
+instance.get(`users/me`).then((response) => response.data);
+
+export const logOut = () => 
+instance.post(`users/log-out`).then(response => response.data)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,37 @@
-import { Box, Button, HStack, IconButton, LightMode, Stack, useColorMode, useColorModeValue, useDisclosure } from "@chakra-ui/react";
+import { Avatar, Box, Button, HStack, IconButton, LightMode, Menu, MenuButton, MenuItem, MenuList, Stack, useColorMode, useColorModeValue, useDisclosure, useToast } from "@chakra-ui/react";
 import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
+import { logOut } from "../api";
+import useUser from "../lib/userUser";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 
 export default function Header() {
+    const { userLoading, isLoggedIn, user } = useUser();
     const{ isOpen:isLoginOpen, onClose:onLoginClose, onOpen:onLoginOpen} = useDisclosure(); // on___ : isOpen을 true or false로 바꾸는 함수
     const{ isOpen:isSignUpOpen, onClose:onSignUpClose, onOpen:onSignUpOpen} = useDisclosure();
     const{ colorMode, toggleColorMode } = useColorMode();
     const logoColor = useColorModeValue("red.500", "red.200"); // 라이트모드일 때는 첫 번째 값, 다크모드일 때는 두 번째 값
     const Icon = useColorModeValue(FaMoon, FaSun); // 컴포넌트에 직접 적용. 대문자로 시작
+
+    const toast = useToast();
+    // BE : api/v1/users/log-out POST 요청임
+    const onLogOut = async() => {
+        const toastId = toast({
+            title: "Login out...",
+            description: "sad to see you go ...",
+            status: "loading",
+            position:"bottom-right"
+        })
+        // const data = await logOut();
+        // console.log(data);
+        setTimeout(() => {
+            toast.update(toastId, {
+                status: "success",
+                title: "Done!",
+                description: "See you later",
+            })
+        }, 5000)
+    }
     return (
         // <HStack justifyContent={"space-between"} py={5} px={40} direction={{ sm:"column", md: "row", }} borderBottomWidth={1}> 내용물의 배치 방향이 바뀌는 경우 HStack, VStack 사용 X 그냥 Stack 사용할 것.
         <Stack justifyContent={"space-between"} alignItems="center" py={5} px={40} direction={{ sm:"column", md: "row", }} spacing={{sm: 4, md: 0}} borderBottomWidth={1}>
@@ -21,10 +44,25 @@ export default function Header() {
                 // icon={colorMode === "light" ? <FaMoon/>: <FaSun />
                 icon={<Icon />}
                 />
-                <Button onClick={onLoginOpen}>Log in</Button>
-                <LightMode> {/* 항상 라이트모드로 고정 */}
-                <Button onClick={onSignUpOpen} colorScheme={"red"}>Sign up</Button>
-                </LightMode>
+                {!userLoading ? (
+                    !isLoggedIn ? (
+                    <>
+                    <Button onClick={onLoginOpen}>Log in</Button>
+                    <LightMode> {/* 항상 라이트모드로 고정 */}
+                    <Button onClick={onSignUpOpen} colorScheme={"red"}>Sign up</Button>
+                    </LightMode>
+                    </>
+                    ) : (
+                        <Menu>
+                            <MenuButton>
+                            <Avatar name={user?.name} src={user?.avatar} size={"md"} />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem onClick={onLogOut}> Log out</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    )
+                ) : null}
             </HStack>
             
             {/* Modal */}
